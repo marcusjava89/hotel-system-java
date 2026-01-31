7f68ad2302f8ffd50903f63d5bfba5d94d9e5f59
Marcus Martins <marcusjava89@gmail.com>
2026-01-31 17:28:39 -0300

    Adds a few logs in CustomresController

--- src/main/java/br/com/hotel/eclipsehotel/controller/CustomerController.java
+++ src/main/java/br/com/hotel/eclipsehotel/controller/CustomerController.java
@@ -12,24 +12,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.hotel.eclipsehotel.model.Customer;
 import br.com.hotel.eclipsehotel.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RestController @RequiredArgsConstructor
+@RestController 
+@RequiredArgsConstructor
 public class CustomerController {
-
+	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 	private final CustomerService service;
 	
 	@GetMapping("/tolist")
 	public ResponseEntity<List<Customer>> toListCustomers(){
+		log.info("List all customers in the system.");
 		List<Customer> customersList = service.toListCustomers();
 		return ResponseEntity.ok(customersList);
 	}
 	
 	@PostMapping("/savecustomer")
 	public ResponseEntity<Customer> saveCustomer(@Valid @RequestBody Customer customer){
+		log.info("Saves one costumer in the system.");
 		Customer saved = service.saveCustomer(customer);
 		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
 	}
